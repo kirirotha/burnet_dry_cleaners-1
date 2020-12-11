61328ee9ca35356dbfbebad4d07f250efacda14a
kirirotha <kirirotha@gmail.com>
2020-12-10 21:52:28 -0600

    updated text

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,7 +7,7 @@ class About extends React.Component {
             
             <div className="content-container">
                 <h2>
-                    We are a locally owned and operated full-service dry cleaning and laundry service. 
+                    We are a locally owned and operated full-service dry cleaning and laundry service. Burnet Dry Cleaners and Laundry has been servicing the Burnet County community for over 40 years.
                 </h2>
             </div>
         )
